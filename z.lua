98d736160cacb812fb9c32e0ec27d7252d3fa36f
goodbyeallfriends <166598480+goodbyeallfriends@users.noreply.github.com>
2024-04-21 14:23:43 +0300

    Update z.lua

--- z.lua
+++ z.lua
@@ -1504,7 +1504,7 @@ end
 				ColorpickerFrame.Visible = false
 				ColorpickerFrame.ClipsDescendants = true
 				ColorpickerFrame.Position = UDim2.new(0.999, 0,0.289, 0)
-				ColorpickerFrame.ZIndex = 331
+				ColorpickerFrame.ZIndex = 5
 				ColorpickerFrame.Size = UDim2.new(0, 175, 0, 0)
 
 				local ColorPickerFrameCorner = Instance.new("UICorner")
